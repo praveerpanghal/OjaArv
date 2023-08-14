d42c73fca4f620da39344c23c22f3982cbff84e6
Praveer Panghal <admin@spinoinc.com>
2023-08-14 12:46:35 -0400

    .

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // URL of the API
-    var apiUrl = "https://toskie-prod-api.azurewebsites.net/ToskieService.svc/GetExceptionlog";
+    var apiUrl = "https://ojaarv-api.onrender.com";
+    const method = "/api/contact";
 
     // Get the container element where data will be displayed
     var dataContainer = $('#data-container');
 
     // AJAX call to fetch data from the API
     $.ajax({
-        url: apiUrl,
+        url: apiUrl + method,
         type: 'GET',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             // Handle the API response data
+            console.log("data",data);
             displayData(data);
         },
-        error: function(error) {
+        error: function (error) {
             dataContainer.text("Error fetching data from the API.");
             console.error(error);
         }
@@ -22,14 +24,10 @@ $(document).ready(function() {
 
     // Function to display API response data
     function displayData(data) {
-        const arrayData=data.GetExceptionlogResult.slice(0, 10);;
-        console.log('data',data.GetExceptionlogResult[0])
-        if (Array.isArray(arrayData)) {
-            var html = '<ul>';
-            arrayData.forEach(function(item) {
-                html += '<li>' + item.ErrorMessage+ '</li>';
-            });
-            html += '</ul>';
+
+        if (data != undefined || data != "") {
+            html = '<li>' + data.fname + "  " + data.lname +'</li>'
+         
             dataContainer.html(html);
         } else {
             dataContainer.text("No data available.");
